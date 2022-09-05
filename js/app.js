001850d3d7aa360bfe362184e348f05ed81bcfd5
ferrari27 <ferrari27>
2022-09-05 16:34:22 -0300

    materialize

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
-let sumaTotal = 0;
-                let ingresosTotal = 0;
-                let gastosTotal = 0;
+//select init
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+});
 
-                function sumar(var1,var2){
-                    sum = parseInt(var1) + parseInt(var2);
-                    return (sum);
-                }
-                function actualizarTotal(){
-                    var signo = document.getElementById("signo").value;
-                    if (signo == "ingreso"){
-                        var valorAgregar = Math.abs(document.getElementById("agregar").value);
-                        ingresosTotal = sumar(ingresosTotal, valorAgregar);
-                    } else {
-                        var valorAgregar = -Math.abs(document.getElementById("agregar").value);  
-                        gastosTotal = sumar(gastosTotal, valorAgregar);  
-                    }
-                    sumaTotal = sumar(sumaTotal,valorAgregar);
-                    document.getElementById("total").innerText = sumaTotal;
-                    document.getElementById("ingresos").innerText = "+"+ingresosTotal;
-                    document.getElementById("gastos").innerText = gastosTotal;
-                }
+//app
+let sumaTotal = 0;
+let ingresosTotal = 0;
+let gastosTotal = 0;
+
+function sumar(var1, var2) {
+    sum = parseInt(var1) + parseInt(var2);
+    return (sum);
+}
+function actualizarTotal() {
+    var signo = document.getElementById("signo").value;
+    if (signo == "ingreso") {
+        var valorAgregar = Math.abs(document.getElementById("agregar").value);
+        ingresosTotal = sumar(ingresosTotal, valorAgregar);
+    } else {
+        var valorAgregar = -Math.abs(document.getElementById("agregar").value);
+        gastosTotal = sumar(gastosTotal, valorAgregar);
+    }
+    sumaTotal = sumar(sumaTotal, valorAgregar);
+    document.getElementById("total").innerText = sumaTotal;
+    document.getElementById("ingresos").innerText = "+" + ingresosTotal;
+    document.getElementById("gastos").innerText = gastosTotal;
+}
